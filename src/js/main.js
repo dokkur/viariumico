36f7e2d8b02db1f1bcefe5bfe20b192aab3286f1
Zemtsov Igor <ZemtsovIgor@yahoo.com>
2017-12-19 16:59:56 +0300

    start version

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,28 @@ import countdown from "jquery-countdown";
 
 $(document).ready(function(){
 
+  /* language selector */
+    $('#langBtn').click(function(event) {
+      var actualLang = $(event.target).data().lang;
+      var langPanel = $(event.target).parent().find('#langPanel');
+      var actualLangItem = langPanel.children().children("[data-lang=" + actualLang + "]");
+      actualLangItem.addClass('-active');
+      actualLangItem.detach().sort();
+      langPanel.children().prepend(actualLangItem);
+      langPanel.addClass('-visible');
+    });
+
+    $('#langPanel').click(function(event) {
+      event.preventDefault();
+      if ($(event.target).parent().data().lang) {
+        $('#langBtn').data('lang', $(event.target).parent().data().lang);
+        $('#langBtn').text($(event.target).parent().text());
+      }
+      $('#langPanel').removeClass('-visible');
+      $('#langPanel').find('li').removeClass('-active');
+    });
+  /**/
+
   /* count down */
     var endTime = "2018-01-20T16:00:00+00:00";
     var formattedEndTime = moment(new Date(endTime)).format('YYYY/MM/DD HH:mm');
@@ -28,46 +50,165 @@ $(document).ready(function(){
     });
   /**/
 
-  /* sen form */
-  $('#feedbackForm').submit(function(event) {
-    event.preventDefault();
-    var form = $(event.target).find("#email").serialize();
-    var email = $(event.target).find("#email").val();
-    if (!email) {
-      bootbox.alert({ 
-        title: "Введите ваш E-mail адрес",
-        message: "Похоже вы забыли ввести ваш E-mail адрес ...", 
-        callback: function(){ 
-          console.log('email is empty');
-        }
+  /* send feedback form */
+    $('#feedbackForm').submit(function(event) {
+      event.preventDefault();
+      var form = $(event.target).find("#email").serialize();
+      var email = $(event.target).find("#email").val();
+      if (!email) {
+        bootbox.alert({
+          title: "Введите ваш E-mail адрес",
+          message: "<p>Похоже вы забыли ввести ваш E-mail адрес ...</p>",
+          backdrop: true,
+          callback: function(){ 
+            console.log('email is empty');
+          }
+        });
+        return false;
+      }
+      $.ajax({
+          type: "POST",
+          url: "https://worldwifi.herokuapp.com/create_request",
+          data: form,
+          success: function () {
+              bootbox.alert({
+              title: "Успех!",
+              message: "Ваша заявка успешно отправлена",
+              backdrop: true,
+              callback: function(){ 
+                console.log('succes');
+                $('#feedbackForm').find("#email").val('');
+              }
+            });
+          },
+          error: function () {
+            bootbox.alert({
+              title: "Нам не удалось отправить вашу заявку",
+              message: "Что-то пошло не так...",
+              backdrop: true,
+              callback: function(){ 
+                console.log('server error');
+              }
+            });
+          }
       });
       return false;
-    }
-    $.ajax({
-        type: "POST",
-        url: "https://worldwifi.herokuapp.com/create_request",
-        data: form,
-        success: function () {
-            bootbox.alert({
-            title: "Успех!",
-            message: "Ваша заявка успешно отправлена", 
-            callback: function(){ 
-              console.log('succes');
-              $('#feedbackForm').find("#email").val('');
-            }
-          });
-        },
-        error: function () {
-          bootbox.alert({
-            title: "Нам не удалось отправить вашу заявку",
-            message: "Что-то пошло не так...", 
-            callback: function(){ 
-              console.log('server error');
-            }
-          });
-        }
     });
-    return false;
-  });
+  /**/
+
+  /* modals */
+    /* Buy token */
+      $('#buyToken').click(function(event) {
+          var buyTokenPopUp = bootbox.dialog({
+            title: 'INVEST VIARIUM',
+            message: '<p></p>',
+            backdrop: true,
+          });
+          buyTokenPopUp.find('.bootbox-body').html($('#buyTokenPopUp').clone());
+
+          /* send Buy token form */
+            $(buyTokenPopUp).find('form').submit(function(event) {
+              event.preventDefault();
+              var form = $(event.target).find(".via-form-input").serialize();
+              var email = $(event.target).find(".via-form-input").val();
+              if (!email) {
+                bootbox.alert({
+                  title: "Введите ваш E-mail адрес",
+                  message: "Похоже вы забыли ввести ваш E-mail адрес ...",
+                  backdrop: true,
+                  callback: function(){ 
+                    console.log('email is empty');
+                  }
+                });
+                return false;
+              }
+              $.ajax({
+                  type: "POST",
+                  url: "https://worldwifi.herokuapp.com/create_request",
+                  data: form,
+                  success: function () {
+                      bootbox.alert({
+                      title: "Успех!",
+                      message: "Ваша заявка успешно отправлена",
+                      backdrop: true,
+                      callback: function(){ 
+                        console.log('succes');
+                        $('#feedbackForm').find("#email").val('');
+                      }
+                    });
+                  },
+                  error: function () {
+                    bootbox.alert({
+                      title: "Нам не удалось отправить вашу заявку",
+                      message: "Что-то пошло не так...",
+                      backdrop: true,
+                      callback: function(){ 
+                        console.log('server error');
+                      }
+                    });
+                  }
+              });
+              return false;
+            });
+          /**/
+      });
+    /**/
+
+    /* Get WP */
+      $('#getWP').click(function(event) {
+          var getWpPopUp = bootbox.dialog({
+            title: 'INVEST VIARIUM',
+            message: '<p></p>',
+            backdrop: true,
+          });
+          getWpPopUp.find('.bootbox-body').html($('#getWpPopUp').clone());
+
+          /* send Buy token form */
+            $(getWpPopUp).find('form').submit(function(event) {
+              event.preventDefault();
+              var form = $(event.target).find(".via-form-input").serialize();
+              var email = $(event.target).find(".via-form-input").val();
+              if (!email) {
+                bootbox.alert({
+                  title: "Введите ваш E-mail адрес",
+                  message: "Похоже вы забыли ввести ваш E-mail адрес ...",
+                  backdrop: true,
+                  callback: function(){ 
+                    console.log('email is empty');
+                  }
+                });
+                return false;
+              }
+              $.ajax({
+                  type: "POST",
+                  url: "https://worldwifi.herokuapp.com/create_request",
+                  data: form,
+                  success: function () {
+                      bootbox.alert({
+                      title: "Успех!",
+                      message: "Ваша заявка успешно отправлена",
+                      backdrop: true,
+                      callback: function(){ 
+                        console.log('succes');
+                        $('#feedbackForm').find("#email").val('');
+                      }
+                    });
+                  },
+                  error: function () {
+                    bootbox.alert({
+                      title: "Нам не удалось отправить вашу заявку",
+                      message: "Что-то пошло не так...",
+                      backdrop: true,
+                      callback: function(){ 
+                        console.log('server error');
+                      }
+                    });
+                  }
+              });
+              return false;
+            });
+          /**/
+      });
+    /**/
   /**/
 });
